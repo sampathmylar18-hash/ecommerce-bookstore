83d208778e72d72909f8240b4cd0c3c63f09f897
sampath <sampathmylar.18@gmail.com>
2026-07-29 17:08:39 +0530

    otp fix2

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,18 +1,16 @@
 const nodemailer = require('nodemailer')
 const dns = require('dns')
 
+dns.setDefaultResultOrder('ipv4first')
+
 const transporter = nodemailer.createTransport({
     host : 'smtp.gmail.com',
     port : 465,
     secure : true,
-    
+    family : 4,
     auth : {
         user : process.env.EMAIL_USER,
         pass : process.env.EMAIL_PASS
-    },
-
-        lookup: (hostname, options, callback) => {
-        dns.lookup(hostname, { family: 4 }, callback)
     }
 })
  async function sendEmail(to, subject , text){
